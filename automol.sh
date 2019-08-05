e28d89e2069b8d87f9562c51a1b80f1146fa84ed
S. Clark <sc2195@cam.ac.uk>
2019-08-05 08:20:53 +0000

    Update automol.sh

--- automol.sh
+++ automol.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+  (( i = 1 ))
 for file in nicola_mops/*.mgf; do
-  echo "Reading {$file}"
-  "load {$file}; isosurface molecular map MEP; show isosurface"  > tmp.spt
-  jmol -ion tmp.spt > $file.jvxl
-  echo "Job complete"
+  echo "Reading $file"
+  echo "load $file; isosurface molecular map MEP; show isosurface"  > tmp.spt
+  jmol -ion tmp.spt > nicola_jmol/"$file".jvxl
+  echo "Job complete $i/4618"
+  (( i++ ))
 done
